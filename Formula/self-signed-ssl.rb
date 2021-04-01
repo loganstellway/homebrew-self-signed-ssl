947bac62359b6764cd5c5b699d750dfefb0a0d73
Logan <loganstellway@gmail.com>
2021-04-01 14:34:39 -0700

    Updates self-signed-ssl

--- Formula/self-signed-ssl.rb
+++ Formula/self-signed-ssl.rb
@@ -1,8 +1,8 @@
 class SelfSignedSsl < Formula
   desc "Generate self-signed TLS certificate using OpenSSL"
   homepage "https://github.com/lstellway/self-signed-ssl"
-  url "https://github.com/lstellway/self-signed-ssl/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "614e4b199fbae1c55546ac3f49b10b9a2ea42169c7bf44a04147e5a2deed5e27"
+  url "https://github.com/lstellway/self-signed-ssl/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "32eecd8848f37064918b1cf60d4c915c349dddd9262c06dac41cb8a56121a084"
   license "MIT"
 
   def install
