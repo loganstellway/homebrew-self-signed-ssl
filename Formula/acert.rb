f695709385ddd55d1221059020888e54681a926e
Logan Stellway <8977231+lstellway@users.noreply.github.com>
2021-09-30 19:02:45 -0700

    Update acert.rb

--- Formula/acert.rb
+++ Formula/acert.rb
@@ -2,7 +2,7 @@ class Acert < Formula
   desc "Command-line utility for managing PKI certificates"
   homepage "https://github.com/lstellway/acert"
   url "https://github.com/lstellway/acert/archive/refs/tags/v2021.09.1.tar.gz"
-  sha256 "93baec26efb49903ae9876c64a14c58ba9e9bf1f3306204254cd8f6bc0393802"
+  sha256 "0ef7de95a1770121bc3512ab958f72d6bee1627dc3f0c7a7c9e0e1fba3368a27"
   license "MIT"
 
   depends_on "go" => :build
